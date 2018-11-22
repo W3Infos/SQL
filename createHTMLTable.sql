3c1d9283272f9e20d6448ba4ee10ca491499beeb
Ramesh <rameshw3offshore@gmail.com>
2018-11-22 12:22:16 +0530

    creating html tags
    Dynamic creating html tags with selected data from table in sql

--- createHTMLTable.sql
+++ createHTMLTable.sql
@@ -10,7 +10,7 @@ select
   (select p.name     as 'td' for xml path(''), type),
   (select ''          as 'td' for xml path(''), type)
  from testtable p 
-
+ 
  for xml path('tr')
 
  ----creating 3 columns header in table ends here------
